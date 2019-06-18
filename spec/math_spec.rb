047352fb354c48d257f0f310f78a5d050752aae6
Jamal Farah <moulayjam@gmail.com>
2019-06-18 02:51:01 +0000

    Automatically backed up by Learn

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -10,6 +10,19 @@ def addition(num1, num2)
   puts num1+num2
 end
 
+def subtraction(num1, num2)
+  puts num1-num2
+end
+
+def division(num1, num2)
+  puts num1/num2
+end
+
+def multiplication(num1, num2)
+  puts num1*num2
+end
+
+
 describe "#subtraction" do
   it "subtracts two numbers from each other" do
     expect(subtraction(10,5)).to eq(5)
